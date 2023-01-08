2de79dd5d7a674f0e6b2129bf85a89be8eb86974
marcopiraccini <marco.piraccini@gmail.com>
2023-01-08 13:30:56 +0100

    soluzioni marco

--- esercizio2_corretto.py
+++ esercizio2_corretto.py
@@ -1,15 +1,16 @@
+import sys
+
+minore1 = sys.maxsize
+minore2 = sys.maxsize
 numero = int(input("Numero: "))
-minore1 = numero
-minore2 = minore1
+
 while numero != 0:
-    numero = int(input("Numero: "))
     if numero != 0:
         if numero < minore1 and numero < minore2:
             minore1 = minore2
             minore2 = numero
         elif numero < minore1 and numero > minore2:
             minore1 = numero
-        elif minore2 == minore1:
-            minore2 = numero
-else:
-    print(minore1, minore2)
+    numero = int(input("Numero: "))
+
+print(minore2, minore1)
