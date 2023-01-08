40011b6bb16d093fe1fb194b1fb4e5fb658d3293
ssirad <105271868+ssirad@users.noreply.github.com>
2023-01-08 13:05:44 +0100

    Create esercizio2_corretto.py

---
+++ esercizio2_corretto.py
@@ -0,0 +1,14 @@
+numero = int(input("Numero: "))
+minore1 = numero
+minore2 = minore1
+while numero != 0:
+    numero = int(input("Numero: "))
+    if numero != 0:
+        if numero < minore1 and numero < minore2:
+            minore2 = numero
+        elif numero < minore1 and numero > minore2:
+            minore1 = numero
+        elif minore2 == minore1:
+            minore2 = numero
+else:
+    print(minore1, minore2)
