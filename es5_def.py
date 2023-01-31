6c6e1e109f5b0fd1ac49d5dbe473e87b39814fe9
ssirad <105271868+ssirad@users.noreply.github.com>
2023-01-31 18:34:58 +0100

    Update es5_def.py

--- es5_def.py
+++ es5_def.py
@@ -1,10 +1,15 @@
 a = float(input("Lunghezza: "))
 b = float(input("Profondità: "))
 c = float(input("Altezza: "))
+n = int(input("Numero piani: "))
 
-def volume(a, b, c):
-    area_base = a*b
-    volume = area_base*c
-    print(volume)
-    
-volume(a, b, c)
+
+def volume(a, b, c, n):
+    volum = 0
+    for i in range(n):
+        area_base = a*b
+        v = area_base*c
+        volum += v
+    print(volum)
+
+volume(a, b, c, n)
