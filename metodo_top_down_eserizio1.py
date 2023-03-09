c2da057dab3ee11a0cac89961678642457a1eda6
ssirad <105271868+ssirad@users.noreply.github.com>
2023-03-09 13:49:03 +0100

    Create metodo_top_down_eserizio1.py
    Il prof ha messo la correzione su classroom ma è completamente diversa dalla mia

---
+++ metodo_top_down_eserizio1.py
@@ -0,0 +1,38 @@
+'''
+Esercizio 1: Si vuole calcolare la superficie di un razzo approssimabile come corpo a un cilindro (raggio di base r e altezza h1),
+come testa a un cono (raggio di base sempre r e altezza h2) e con 4 alettoni approssimabili a dei triangoli rettangoli uguali
+(di cateti c1 e c2 e spessore inrilevante).
+'''
+from math import pi, sqrt
+
+def area_alettoni(cat_1, cat_2):
+    #   area triangolo = (b * h)/2
+    area = (cat_1 * cat_2)/2
+    return area
+
+def area_cilindro(raggio, altezza):
+    #   cilindro = (2πr * h) + πr**2
+    area_laterale = raggio * altezza * pi
+    area_tot = area_laterale + pi * raggio**2
+    return area_tot
+def area_cono(raggio, altezza):
+    #   cono = πr * a
+    apotema = sqrt(raggio**2 + altezza**2)
+    area_laterale = apotema * pi * raggio
+    return area_laterale
+
+def main():
+    r = float(input("raggio base: "))
+    h1 = float(input("altezza cilindro: "))
+    h2 = float(input("altezza cono: "))
+    c1 = float(input("cateto 1 alettoni:"))
+    c2 = float(input("cateto 2 alettoni:"))
+
+    alettoni = area_alettoni(c1, c2)
+    cilindro = area_cilindro(r, h1)
+    cono = area_cono(r, h2)
+    area_razzo = alettoni * 4, cilindro, cono
+    print(area_razzo)
+main()
+
+#24 righe in tutto
